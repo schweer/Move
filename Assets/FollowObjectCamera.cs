3def9108b4bc24abe921cc84a65d096cf140784d
Caleb Schweer <calebbsch@gmail.com>
2018-10-22 23:14:55 -0500

    Created Repository. Made a sphere that moves with a camera that follows it.

---
+++ Assets/FollowObjectCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowObjectCamera : MonoBehaviour {
+
+    GameObject game_object;
+    Vector3 position;
+
+    const string object_name = "Player";
+    const float acceleration = 0.1F;
+    const float x_offset = 0F;
+    const float y_offset = 1F;
+    const float z_offset = -5F;
+
+    // Use this for initialization
+    void Start () {
+        game_object = GameObject.Find(object_name);
+
+        position.x = game_object.transform.position.x + x_offset;
+        position.y = game_object.transform.position.y + y_offset;
+        position.z = game_object.transform.position.z + z_offset;
+
+        transform.position = position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        position.x = game_object.transform.position.x + x_offset;
+        position.y = game_object.transform.position.y + y_offset;
+        position.z = game_object.transform.position.z + z_offset;
+
+        transform.position = position;
+    }
+}
---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour {
+
+    Vector3 position;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        position = transform.position;
+        position.y -= (float) 0.01;
+        transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            position = transform.position;
+            position.x--;
+            transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            position = transform.position;
+            position.x++;
+            transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            position = transform.position;
+            position.y++;
+            transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            position = transform.position;
+            position.y--;
+            transform.position = position;
+        }
+    }
+}
