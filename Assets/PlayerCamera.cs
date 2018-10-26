b518318c64a19f41fa1b4e203aab30e534ebc97f
Caleb Schweer <calebbsch@gmail.com>
2018-10-25 23:06:58 -0500

    Added a camera that moves and movement based on camera direction. yeet

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject player;
+    public Camera player_camera;
+
+    public Vector3 offset;
+
+    public float rotation_speed = 3.0F;
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        player_camera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    void OnGUI()
+    {
+        Vector3 point = new Vector3();
+        Event currentEvent = Event.current;
+        Vector2 mousePos = new Vector2();
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = player_camera.pixelHeight - currentEvent.mousePosition.y;
+
+        point = player_camera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, player_camera.nearClipPlane));
+
+        GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+        GUILayout.Label("Screen pixels: " + player_camera.pixelWidth + ":" + player_camera.pixelHeight);
+        GUILayout.Label("Mouse position: " + mousePos);
+        GUILayout.Label("World position: " + point.ToString("F3"));
+        GUILayout.EndArea();
+
+        Quaternion neededRotation = Quaternion.LookRotation(point - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, neededRotation, Time.deltaTime * rotation_speed);
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 6.0F;
+    public float jump_speed = 8.0F;
+    public float gravity = 20.0F;
+    public Vector3 move_direction = Vector3.zero;
+    public Camera player_camera;
+    public CharacterController controller;
+
+    private void Start()
+    {
+        player_camera = GameObject.Find("PlayerCamera").GetComponent<Camera>();
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + new Vector3(player_camera.transform.forward.x, 0, player_camera.transform.forward.z) * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(-player_camera.transform.right.x, 0, -player_camera.transform.right.z) * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + new Vector3(-player_camera.transform.forward.x, 0, -player_camera.transform.forward.z) * speed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + new Vector3(player_camera.transform.right.x, 0, player_camera.transform.right.z) * speed * Time.deltaTime;
+        }
+
+        if (controller.isGrounded)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                move_direction.y = jump_speed;
+            }
+        }
+
+        move_direction.y -= gravity * Time.deltaTime;
+        controller.Move(move_direction * Time.deltaTime);
+    }
+}
