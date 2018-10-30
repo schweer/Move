87fcce7e8b97b595f9da5f03a5ede30026f5ba33
Caleb Schweer <calebbsch@gmail.com>
2018-10-30 16:10:14 -0500

    Got some snappy movement. Jump speed is weird.

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private float look_sensitivity = 2.0F;
+    private float max_look_angle = 75;
+    private float min_look_angle = -75;
+    private GameObject look_object;
+    private Vector3 offset;
+    private float horizontal_input, vertical_input;
+    private Quaternion camera_quaternion;
+
+    void Start()
+    {
+        look_object = GameObject.Find("PlayerController");
+        offset = transform.position - look_object.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        horizontal_input += Input.GetAxis("Mouse X") * look_sensitivity;
+        vertical_input -= Input.GetAxis("Mouse Y") * look_sensitivity;
+
+        if (vertical_input > 0)
+        {
+            vertical_input = Mathf.Min(max_look_angle, vertical_input);
+        }
+        else
+        {
+            vertical_input = Mathf.Max(min_look_angle, vertical_input);
+        }
+
+        transform.position = look_object.transform.position + offset;
+
+        transform.rotation = Quaternion.Euler(Mathf.Min(Mathf.Max(vertical_input, -75), 75), horizontal_input, 0);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// The GameObject is made to bounce using the space key.
+// Also the GameOject can be moved forward/backward and left/right.
+// Add a Quad to the scene so this GameObject can collider with a floor.
+
+public class PlayerController : MonoBehaviour
+{
+    public float current_speed_x = 0.0f;
+    public float current_speed_z = 0.0f;
+    public float acceleration = 100.0f;
+    public float max_speed = 30.0f;
+    public float jump_speed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 move_direction = Vector3.zero;
+    private CharacterController controller;
+    private float horizontal_input, vertical_input;
+
+    private KeyCode left_key = KeyCode.A;
+    private KeyCode right_key = KeyCode.D;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        horizontal_input = Input.GetAxis("Horizontal");
+        vertical_input = Input.GetAxis("Vertical");
+
+        if (horizontal_input > 0)
+        {
+            current_speed_x = Mathf.Min(horizontal_input * acceleration, max_speed);
+        }
+        if (vertical_input > 0)
+        {
+            current_speed_z = Mathf.Min(vertical_input * acceleration, max_speed);
+        }
+        if (horizontal_input < 0)
+        {
+            current_speed_x = Mathf.Max(horizontal_input * acceleration, -max_speed);
+        }
+        if (vertical_input < 0)
+        {
+            current_speed_z = Mathf.Max(vertical_input * acceleration, -max_speed);
+        }
+        if (horizontal_input == 0)
+        {
+            current_speed_x = horizontal_input;
+        }
+        if (vertical_input == 0)
+        {
+            current_speed_z = vertical_input;
+        }
+
+        move_direction.x = current_speed_x;
+        move_direction.z = current_speed_z;
+
+        if (Input.GetButton("Jump") && controller.isGrounded)
+        {
+            move_direction.y = jump_speed;
+        }
+
+        move_direction.y -= (gravity * Time.deltaTime);
+        
+        controller.Move(move_direction * Time.deltaTime);
+    }
+}
