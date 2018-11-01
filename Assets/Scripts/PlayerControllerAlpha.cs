5c94c396310470ac5a163efdf3fdcf02fdeb9fa3
Caleb Schweer <calebbsch@gmail.com>
2018-10-31 19:19:37 -0500

    Renamed PlayerController.cs to PlayerControllerAlpha.cs as a reference for a new controller from scratch.

--- Assets/Scripts/PlayerControllerAlpha.cs
+++ Assets/Scripts/PlayerControllerAlpha.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 //test
 
-public class PlayerController : MonoBehaviour
+public class PlayerControllerAlpha : MonoBehaviour
 {
     public float current_speed_x = 0.0f;
     public float current_speed_z = 0.0f;
@@ -43,17 +43,17 @@ public class PlayerController : MonoBehaviour
 
         ground_ray.origin = transform.position;
             
-        grounded = Physics.Raycast(ground_ray, out ground_ray_hit, controller.bounds.extents.y * 1.1f); // ground check
+        grounded = Physics.Raycast(ground_ray, out ground_ray_hit, controller.bounds.extents.y * 1.5f); // ground check
         Physics.Raycast(ground_ray, out ground_ray_hit, Mathf.Infinity); // ground angle check
 
-        angle = Vector3.Angle(ground_ray_hit.normal, -transform.up) - 180;
+        angle = Mathf.Abs(Vector3.Angle(ground_ray_hit.normal, -transform.up) - 180);
 
         if (Input.GetKey(KeyCode.Escape))
         {
             Cursor.lockState = CursorLockMode.None;
         }
 
-        if (grounded)
+        if (controller.isGrounded)
         {
             acceleration = ground_acceleration;
         }
@@ -65,84 +65,65 @@ public class PlayerController : MonoBehaviour
         horizontal_input = Input.GetAxis("Horizontal");
         vertical_input = Input.GetAxis("Vertical");
 
-        /*
-        move_direction.x = (transform.right.x * horizontal_input * acceleration) + (transform.forward.x * vertical_input * acceleration);
-        move_direction.z = (transform.right.z * horizontal_input * acceleration) + (transform.forward.z * vertical_input * acceleration);
-        */
-
         if (horizontal_input > 0)
         {
             current_speed_x = Mathf.Min(horizontal_input * acceleration, max_speed);
-            //move_direction.x = transform.right.x * horizontal_input * acceleration;
         }
         if (vertical_input > 0)
         {
             current_speed_z = Mathf.Min(vertical_input * acceleration, max_speed);
-            //move_direction.z = transform.forward.z * vertical_input * acceleration;
         }
         if (horizontal_input < 0)
         {
             current_speed_x = Mathf.Max(horizontal_input * acceleration, -max_speed);
-            //move_direction.x = transform.right.x * horizontal_input * acceleration;
         }
         if (vertical_input < 0)
         {
             current_speed_z = Mathf.Max(vertical_input * acceleration, -max_speed);
-            //move_direction.z = transform.forward.z * vertical_input * acceleration;
         }
         if (horizontal_input == 0)
         {
             current_speed_x = horizontal_input;
-            //move_direction.x = transform.right.x * horizontal_input * acceleration;
         }
         if (vertical_input == 0)
         {
             current_speed_z = vertical_input;
-            //move_direction.z = transform.forward.z * vertical_input * acceleration;
         }
 
         move_direction.x = (transform.right.x * current_speed_x) + (transform.forward.x * current_speed_z);
         move_direction.z = (transform.right.z * current_speed_x) + (transform.forward.z * current_speed_z);
 
-        /*
-        move_direction.x = current_speed_x;
-        move_direction.z = current_speed_z;
-        */
-
-        /*
-        if (Input.GetKey(dash_key) && move_direction.x > 0)
+        if (grounded)
         {
-            move_direction.x += dash_distance;
-        }
-        if (Input.GetKey(dash_key) && move_direction.x < 0)
-        {
-            move_direction.x -= dash_distance;
-        }
-        if (Input.GetKey(dash_key) && move_direction.z > 0)
-        {
-            move_direction.z += dash_distance;
-        }
-        if (Input.GetKey(dash_key) && move_direction.z < 0)
-        {
-            move_direction.z -= dash_distance;
-        }
-        */
-
-        if (grounded && move_direction.y < 0)
-        {
-            move_direction.y = 0f; // Keeps alternating between grounded and not grounded
+            move_direction.y = 0;
         }
 
-        if ((grounded || controller.isGrounded) && Input.GetButton("Jump"))
+        if (grounded && Input.GetButton("Jump"))
         {
             move_direction.y = jump_speed;
         }
 
-        if (!grounded || controller.isGrounded)
+        if (!grounded)
         {
-            move_direction.y -= (gravity * Time.deltaTime);
+            move_direction.y -= gravity * Time.deltaTime;
         }
-        
+
+        /*
+        if (controller.isGrounded)
+        {
+            move_direction.y = -1f;
+        }
+
+        if (controller.isGrounded && Input.GetButton("Jump"))
+        {
+            move_direction.y = jump_speed;
+        }
+
+        if (!controller.isGrounded)
+        {
+            move_direction.y -= gravity * Time.deltaTime;
+        }*/
+
         controller.Move(move_direction * Time.deltaTime);
 
         /*
@@ -161,7 +142,7 @@ public class PlayerController : MonoBehaviour
     private void RunDebuggers()
     {
         //Debug.Log("move_direction.x, move_direction.z: " + move_direction.x + ", " + move_direction.z);
-        Debug.Log("grounded: " + grounded + " isGrounded: " + controller.isGrounded + " angle: " + angle + " magnitude: " + move_direction.magnitude);
+        Debug.Log("grounded: " + grounded + " controller.isGrounded: " + controller.isGrounded + " angle: " + angle + " ground_ray_hit.point: " + ground_ray_hit.point + " move_direction.y: " + move_direction.y);
 
         // draw ground_ray
         //Debug.DrawRay(transform.position, Vector3.down * controller.bounds.extents.y * 1.1f, Color.red, 1.0f);
