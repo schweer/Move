a697c2d092ef5375f184afd03d59d86085896cc4
Caleb Schweer <calebbsch@gmail.com>
2018-11-01 00:30:42 -0500

    Slopes are pretty smooth now.

--- Assets/Scripts/PlayerControllerBeta.cs
+++ Assets/Scripts/PlayerControllerBeta.cs
@@ -10,6 +10,7 @@ public class PlayerControllerBeta : MonoBehaviour {
     private float current_speed_x, current_speed_z;
     private float acceleration = 100;
     private float max_speed = 15;
+    private float gravity = 30;
 
     Vector3 move_direction;
     Ray ground_ray;
@@ -23,17 +24,18 @@ public class PlayerControllerBeta : MonoBehaviour {
     {
         controller = GetComponent<CharacterController>();
         ground_angle = transform.GetChild(0);
-        ground_ray.direction = -transform.up;
-        //Debug.Log("ground_angle: " + ground_angle + " controller: " + controller);
+        ground_ray = new Ray(transform.position, -transform.up);
+        Debug.Log("ground_angle: " + ground_angle + " controller: " + controller);
 	}
-	
-	private void Update ()
+
+    private void Update()
     {
         ground_ray.origin = transform.position;
-        //Physics.Raycast(ground_ray, out ground_ray_hit, Mathf.Infinity);
-        Physics.SphereCast(ground_ray, 0.2f, out ground_ray_hit, Mathf.Infinity);
+        Physics.Raycast(ground_ray, out ground_ray_hit, Mathf.Infinity);
+        //Physics.SphereCast(ground_ray, 0.2f, out ground_ray_hit, Mathf.Infinity); // Mess with this if character models are too wide for gaps they move in.
+        angle = Vector3.Angle(ground_ray_hit.normal, Vector3.up);
 
-        ground_angle.transform.rotation = Quaternion.FromToRotation(Vector3.up, ground_ray_hit.normal) * transform.rotation;
+        ground_angle.rotation = Quaternion.FromToRotation(Vector3.up, ground_ray_hit.normal) * transform.rotation;
 
         horizontal_input = Input.GetAxis("Horizontal");
         vertical_input = Input.GetAxis("Vertical");
@@ -63,22 +65,27 @@ public class PlayerControllerBeta : MonoBehaviour {
             current_speed_z = vertical_input;
         }
 
-        move_direction.x = (ground_angle.transform.right.x * current_speed_x) + (ground_angle.transform.forward.x * current_speed_z);
-        if(controller.isGrounded) move_direction.y = (ground_angle.transform.right.y * current_speed_x) + (ground_angle.transform.forward.y * current_speed_z);
-        move_direction.z = (ground_angle.transform.right.z * current_speed_x) + (ground_angle.transform.forward.z * current_speed_z);
-
-        if (!controller.isGrounded)
+        move_direction.x = (ground_angle.right.x * current_speed_x) + (ground_angle.forward.x * current_speed_z);
+        if(controller.isGrounded || (angle != 0 && Vector3.Distance(transform.position, ground_ray_hit.point) < 0.5f)) move_direction.y = (ground_angle.right.y * current_speed_x) + (ground_angle.forward.y * current_speed_z);
+        move_direction.z = (ground_angle.right.z * current_speed_x) + (ground_angle.forward.z * current_speed_z);
+        
+        if (controller.isGrounded)
         {
-            move_direction.y -= 140 * Time.deltaTime;
+            move_direction.y = -0.1f;
         }
 
-        //Debug.Log(ground_ray_hit.normal);
-        Debug.DrawRay(ground_angle.position, ground_angle.forward, Color.blue, 2);
+        if (!controller.isGrounded && Vector3.Distance(transform.position, ground_ray_hit.point) > 0.5f)
+        {
+            move_direction.y -= gravity * Time.deltaTime;
+        }
+        
+        Debug.Log("controller.isGrounded: " + controller.isGrounded + " move_direction.y: " + move_direction.y + " angle: " + angle);
         Debug.DrawRay(ground_angle.position, ground_angle.right, Color.red, 2);
         Debug.DrawRay(ground_angle.position, ground_angle.up, Color.green, 2);
+        Debug.DrawRay(ground_angle.position, ground_angle.forward, Color.blue, 2);
 
         controller.Move(move_direction * Time.deltaTime);
-	}
+    }
 
     private void LateUpdate()
     {
